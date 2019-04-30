30b1c1230f203bf793456c6e499dfd40c793dec5
msulman147 <msulman147@gmai.com>
2019-04-30 17:26:41 +0500

    Adding Responsive Nav Bar

--- src/components/subcomponents/Header.js
+++ src/components/subcomponents/Header.js
@@ -1,10 +1,15 @@
 
 import React from 'react';
+import { Navbar , Nav ,Form ,FormControl } from 'react-bootstrap' ;
 import { NavLink} from 'react-router-dom';
 import  '../../styles/styles.scss';
 import Button from 'react-bootstrap/Button';
 
-
+const Style = {
+    backgroundColor:'rgb(255, 196, 35)',
+    marginTop:'25px',
+    padding:'0'
+  };
 const  Header = () => (
     <div>
         <div className="Logo-box">
@@ -16,14 +21,25 @@ const  Header = () => (
                <Button variant="warning">REQUET A QUOTE</Button>
             </div>
         </div>
-    <header className="head" >
-    <NavLink to="/" activeClassName="is-active" exact= {true} className="headNav">HOME </NavLink>
-    <NavLink to="/About" activeClassName="is-active" className="headNav">ABOUT US</NavLink>
-    <NavLink to="/Products" activeClassName="is-active" className="headNav">PRODUCTS</NavLink>
-    <NavLink to="/Groupcompanies" activeClassName="is-active" className="headNav"> GROUP COMPANIES</NavLink>
-    
-    <NavLink to="/ContactUs" activeClassName="is-active" className="headNav"> CONTACT US</NavLink>
-    </header>
+
+
+
+
+
+        <Navbar collapseOnSelect expand="xl" bg="rgb(34, 35, 48)" style={Style}>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="head" >
+                    <NavLink to="/" activeClassName="is-active" exact= {true} className="headNav">HOME </NavLink>
+                    <NavLink to="/About" activeClassName="is-active" className="headNav">ABOUT US</NavLink>
+                    <NavLink to="/Products" activeClassName="is-active" className="headNav">PRODUCTS</NavLink>
+                    <NavLink to="/Groupcompanies" activeClassName="is-active" className="headNav"> GROUP COMPANIES</NavLink>
+                    <NavLink to="/ContactUs" activeClassName="is-active" className="headNav"> CONTACT US</NavLink>
+                </Nav>
+               
+            </Navbar.Collapse>
+        </Navbar>
+         
     </div>
 );
 
